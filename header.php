43e26767c346f890d74d0e80623cf958133e63cf
Weston Ruter <weston@xwp.co>
2018-01-12 06:53:00 -0800

    Integrate with canonical AMP support provided by plugin
    * Add amp html doctype and update viewport meta tag
    * Remove HTML head tags now output via AMP plugin; remove illegal CSS

--- header.php
+++ header.php
@@ -11,13 +11,10 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html amp <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 </head>
 
 <body <?php body_class(); ?>>
