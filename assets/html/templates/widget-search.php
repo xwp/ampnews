a1a1c92086c51fc8849fc9ff064a2bf6eddee822
Piotr Delawski <piotr.delawski@xwp.co>
2018-01-19 13:00:29 +0100

    Issue 21: Add search widget styling and update markup.

--- assets/html/templates/widget-search.php
+++ assets/html/templates/widget-search.php
@@ -9,12 +9,16 @@
 ?>
 
 <section id="search-2" class="widget widget_search">
-	<form role="search" method="get" class="search-form i-amphtml-form" action="http://ampconf.local/"
-	      novalidate="">
+	<form
+			role="search"
+			method="get"
+			class="search-form"
+			action="https://ampconf.local/"
+			target="_top">
 		<label>
 			<span class="screen-reader-text">Search for:</span>
-			<input type="search" class="search-field" placeholder="Search …" value="" name="s">
+			<input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" />
 		</label>
-		<input type="submit" class="search-submit" value="Search">
+		<input type="submit" class="search-submit" value="Search" />
 	</form>
 </section>
