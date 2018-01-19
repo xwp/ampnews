a74ff72e1055ee92e3d5c84be811b5bf5bbb8fd8
Piotr Delawski <piotr.delawski@xwp.co>
2018-01-19 12:12:56 +0100

    Issue 21: Update audio widget markup so that it's a valid AMP code.

--- assets/html/templates/widget-media-audio.php
+++ assets/html/templates/widget-media-audio.php
@@ -8,21 +8,30 @@
 // @codingStandardsIgnoreStart
 ?>
 
-<section id="media_audio-1" class="widget widget_media_audio"><h2 class="widget-title">Test Audio Widget: With Loop,
-		Preload</h2><!--[if lt IE 9]>
-	<script>document.createElement( 'audio' );</script><![endif]-->
-	<audio class="wp-audio-shortcode" id="audio-903-1" loop="1" preload="auto" style="width: 100%;"
-		   controls="controls">
-		<source type="audio/mpeg" src="http://ampconf.local/wp-content/uploads/2013/03/Eddy-Need-Remix.mp3?_=1">
-		<a href="http://ampconf.local/wp-content/uploads/2013/03/Eddy-Need-Remix.mp3">http://ampconf.local/wp-content/uploads/2013/03/Eddy-Need-Remix.mp3</a>
-	</audio>
-</section>
-
-<section id="media_audio-2" class="widget widget_media_audio"><h2 class="widget-title">Test Audio Widget: With Loop,
-		Preload</h2>
-	<audio class="wp-audio-shortcode" id="audio-903-2-2" loop="1" preload="auto" style="width: 100%;"
-		   controls="controls">
-		<source type="audio/mpeg" src="http://ampconf.local/wp-content/uploads/2013/03/Eddy-Need-Remix.mp3?_=2">
-		<a href="http://ampconf.local/wp-content/uploads/2013/03/Eddy-Need-Remix.mp3">http://ampconf.local/wp-content/uploads/2013/03/Eddy-Need-Remix.mp3</a>
-	</audio>
-</section>
+<div id="media_audio-1" class="widget widget_media_audio">
+	<h2 class="widget-title">Test Audio Widget: With Loop, Preload</h2>
+	<?php
+	/**
+	 * Dev note
+	 *
+	 * Even though AMP reference says `preload` attribute is allowed, I was getting
+	 * validation error whenever it was present in the `amp-audio` tag.
+	 *
+	 * @see https://www.ampproject.org/docs/reference/components/amp-audio
+	 */
+	?>
+	<amp-audio
+			class="wp-audio-shortcode"
+			id="audio-903-1"
+			width="auto"
+			height="50"
+			loop
+			controls>
+		<source type="audio/mpeg" src="https://open.live.bbc.co.uk/mediaselector/5/redir/version/2.0/mediaset/audio-nondrm-download/proto/http/vpid/p05tnfn8.mp3">
+		<div fallback>
+			<a href="https://open.live.bbc.co.uk/mediaselector/5/redir/version/2.0/mediaset/audio-nondrm-download/proto/http/vpid/p05tnfn8.mp3">
+				https://open.live.bbc.co.uk/mediaselector/5/redir/version/2.0/mediaset/audio-nondrm-download/proto/http/vpid/p05tnfn8.mp3
+			</a>
+		</div>
+	</amp-audio>
+</div>
