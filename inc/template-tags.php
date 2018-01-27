6d9c1385ed331ba6e25f12a9a5a5fe37224f0392
Justin Kopepasah <justin@kopepasah.com>
2018-01-27 14:02:53 -0600

    32 Add todo for creating image size markup.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -106,41 +106,13 @@ endif;
 if ( ! function_exists( 'ampconf_post_thumbnail' ) ) :
 	/**
 	 * Displays an optional post thumbnail.
-	 *
-	 * Wraps the post thumbnail in an anchor element on index views, or a div
-	 * element when on single views.
 	 */
 	function ampconf_post_thumbnail() {
 		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
 			return;
 		}
 
-		if ( is_singular() ) :
-		?>
-
-		<div class="post-thumbnail">
-		<?php the_post_thumbnail(); ?>
-	</div><!-- .post-thumbnail -->
-
-	<?php else : ?>
-
-	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
-		<?php
-			the_post_thumbnail(
-				'post-thumbnail',
-				array(
-					'alt' => the_title_attribute(
-						array(
-							'echo' => false,
-						)
-					),
-				)
-			);
-		?>
-	</a>
-
-	<?php
-	endif; // End is_singular().
+		// TODO: Create custom markup for ampconf's specific image sizes.
 	}
 endif;
 
