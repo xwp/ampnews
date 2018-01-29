d2b1a1dda2450d6d9c8730005b0f45b18fa48bc8
Justin Kopepasah <justin@kopepasah.com>
2018-01-29 13:33:28 -0600

    39 Change to use entry__content class.

--- templates/entry/full.php
+++ templates/entry/full.php
@@ -19,7 +19,7 @@
 		<?php the_title( '<h2 class="entry__title">', '</h2>' ); ?>
 	</header><!-- .entry__header -->
 
-	<div class="entry__summary">
+	<div class="entry__content">
 		<p><?php the_content(); ?></p>
 	</div><!-- .entry__summary -->
 
