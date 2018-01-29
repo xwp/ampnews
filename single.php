2f34c9110543bb5717f4220e91ef752edd82cee1
Justin Kopepasah <justin@kopepasah.com>
2018-01-28 23:03:47 -0600

    39 Integrate single post template.

--- single.php
+++ single.php
@@ -9,28 +9,41 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+	<?php
+	while ( have_posts() ) :
+		the_post();
 		?>
+		<div class="wrap">
+			<main class="wrap__item wrap__item--full-width">
+				<?php get_template_part( 'templates/entry/full' ); ?>
+			</main>
+		</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<div class="wrap wrap--single">
+			<div class="wrap__item wrap__item--heading">
+				<h3 class="heading heading--widget"><?php esc_html_e( 'More Stories', 'ampconf' ); ?></h3>
+			</div>
+
+			<?php // TODO: Create loop of recent posts, cached. ?>
+			<div class="wrap__item">
+				<?php get_template_part( 'templates/entry/slim' ); ?>
+			</div>
+
+			<div class="wrap__item wrap__item--heading">
+				<h3 class="heading heading--widget"><?php esc_html_e( 'Comments', 'ampconf' ); ?></h3>
+			</div>
+
+			<div class="wrap__item">
+				<?php
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+				?>
+			</div>
+		</div>
+		<?php
+	endwhile;
+	?>
 
 <?php
-get_sidebar();
 get_footer();
