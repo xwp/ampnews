a0ed3ed7e998ccc9e198e86cbcc972c7a9651023
Weston Ruter <weston@xwp.co>
2018-01-29 23:38:45 -0800

    Include amp-live-list in comments template

--- comments.php
+++ comments.php
@@ -22,56 +22,48 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-	?>
-		<h2 class="comments-title">
-			<?php
-			$comment_count = get_comments_number();
-			if ( 1 === $comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'ampconf' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'ampconf' ) ),
-					number_format_i18n( $comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
+	<?php /* @todo Set initial class names based on have_comments() */ ?>
+	<h2 class="comments-title">
+		<?php
+		$comment_count = get_comments_number();
+		if ( '1' === $comment_count ) {
+			printf(
+				/* translators: 1: title. */
+				esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'ampconf' ),
+				'<span>' . get_the_title() . '</span>'
+			);
+		} else {
+			printf( // WPCS: XSS OK.
+				/* translators: 1: comment count number, 2: title. */
+				esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'ampconf' ) ),
+				number_format_i18n( $comment_count ),
+				'<span>' . get_the_title() . '</span>'
+			);
+		}
+		?>
+	</h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
-
-		<ol class="comment-list">
+	<amp-live-list id="amp-live-comments-list-<?php the_ID(); ?>" layout="container" data-poll-interval="15000" data-max-items-per-page="<?php echo esc_attr( get_option( 'page_comments' ) ? get_option( 'comments_per_page' ) : 10000 ); ?>">
+		<div update>
+			<button update on="tap:amp-live-comments-list-<?php the_ID(); ?>.update"><?php esc_html_e( 'You have updates', 'ampconf' ); ?></button>
+		</div>
+		<ol items class="comment-list">
 			<?php
-				wp_list_comments(
-					array(
-						'style'      => 'ol',
-						'short_ping' => true,
-					)
-				);
+				wp_list_comments( array(
+					'style'      => 'ol',
+					'short_ping' => true,
+				) );
 			?>
 		</ol><!-- .comment-list -->
+		<nav pagination>
+			<?php the_comments_navigation(); ?>
+		</nav>
+	</amp-live-list>
 
-		<?php
-		the_comments_navigation();
+	<?php if ( ! comments_open() ) : ?>
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'ampconf' ); ?></p>
+	<?php endif; ?>
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-		?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'ampconf' ); ?></p>
-		<?php
-		endif;
-
-	endif; // Check for have_comments().
-
-	comment_form();
-	?>
+	<?php comment_form(); ?>
 
 </div><!-- #comments -->
